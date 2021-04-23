6556e643ebc69799c8ee1645f1f86dfc9a09b4dc
Ruslan <ruslanmirzaev16621@gmail.com>
2021-04-24 03:52:01 +0700

    Версия 2.0

--- logic.py
+++ logic.py
@@ -2,13 +2,44 @@ import telebot
 import gspread
 import datetime
 import calendar
+import time
+import re
+from selenium import webdriver
 from bs4 import BeautifulSoup
+from selenium.webdriver.common.keys import Keys
 from datetime import date, timedelta
 
 gc = gspread.service_account(filename="creds.json")
 sh = gc.open("Расписание")
 bot = telebot.TeleBot("1799617235:AAH6YI3rdpbthgXvZxryYNWzQzxzl11cbXc")
 koordinateCell = {"Программирование (лаб) 9:45-11:20": "A3", "Исит (лаб) 11:45-13:20": "A4"}
+driver = webdriver.Chrome('chromedriver.exe')
+programm , icit = "", ""
+
+def parseDedline(needSubject):
+    time.sleep(5)
+    subject = driver.find_elements_by_xpath("//input[@title = 'Выбрать']")
+    indexSubjectButton = {"Программирование": 13, "Исит": 12} 
+    subject[indexSubjectButton[needSubject]].click()
+    time.sleep(4)
+    htmlContent = BeautifulSoup(driver.page_source, "lxml")
+    allElementParse = ""
+    for el in htmlContent.find_all("td", class_= False):
+        allElementParse += el.get_text(strip = True)
+    allElementParse  = re.findall(r"\d{2}-\d{2}-\d{4}", allElementParse)
+    driver.find_element_by_xpath('//input[@value="Вернуться"]').send_keys(Keys.ENTER)
+    time.sleep(4)
+    return allElementParse
+
+def authorization(login, password):
+    driver.get('https://eios.kemsu.ru/a/eios')
+    time.sleep(5)
+    driver.find_element_by_name('username').send_keys("stud67266")
+    driver.find_element_by_name('password').send_keys("bZqb11Fy")
+    driver.find_element_by_class_name("css-h0m9oy").send_keys(Keys.ENTER)
+    time.sleep(5)
+    linkInfo = driver.find_element_by_link_text('Информационное обеспечение учебного процесса (ИнфОУПро)')
+    driver.get(linkInfo.get_attribute("href"))
 
 def determineParity(currentDate): # определяет четность недели
     year = currentDate.year if currentDate.month >= 9 else currentDate.year - 1
@@ -19,35 +50,29 @@ def determineParity(currentDate): # определяет четность нед
         return "Нечетная неделя"
     return "Четная неделя"  
 
+
 def clearDedline():
     for key in koordinateCell.keys():
         sh.worksheet("Нечетная неделя").update(koordinateCell[key], key)
         sh.worksheet("Четная неделя").update(koordinateCell[key], key)
 
+
 worksheet = sh.worksheet(determineParity(date.today())) 
 clearDedline()
-def parse(path, htmlEl, className): # парсит из определенного html контента дедлайн и возращает отформатированный список словарей с датами
-    with open(path, encoding= 'utf-8') as file:
-        fileParse = file.read()
-    soup = BeautifulSoup(fileParse, "lxml")
-    htmlContent = soup.find_all(htmlEl, class_= className)
-    dedline = {"День": 0, "Месяц": 0, "Год": 0}
+
+def parse(subjectParse): # парсит из определенного html контента дедлайн и возращает отформатированный список словарей с датами
     allDedlineList = []
-    for el in htmlContent:
-        text = el.text.split("-")
-        for i in range(0, len(text)):
-            dedline[list(dedline)[i]] = int(text[i])
-        allDedlineList.append(dedline.copy()) 
-    return allDedlineList 
+    for el in range(0, len(subjectParse)):
+        text = subjectParse[el].split("-")
+        allDedlineList.append(datetime.date(int(text[2]), int(text[1]), int(text[0])))
+    return sorted(allDedlineList) 
 
 
-def write(path, htmlEl, className, itemName, currDate): # записывает в google shets дедлайны
-    dedline = parse(path, htmlEl, className)
-    for el in dedline: 
-        if currDate.day <= el["День"] and currDate.month <= el["Месяц"] and currDate.year <= el["Год"]:
-            stateWeek = determineParity(datetime.date(el["Год"], el["Месяц"], el["День"]))           
-            textCell = f"{itemName} {el['День']}.{el['Месяц']}.{el['Год']}"
-            sh.worksheet(stateWeek).update(koordinateCell[itemName], textCell)
+def write(itemName, currDate, subjectPars): # записывает в google shets дедлайны
+    for el in range(0, len(subjectPars)):
+        if subjectPars[el] >= currDate:
+            stateWeek = determineParity(subjectPars[el])           
+            sh.worksheet(stateWeek).update(koordinateCell[itemName], itemName + " " + subjectPars[el].strftime("%d.%m.%Y"))
             break
               
 def beauPrint(objStr):
@@ -55,12 +80,15 @@ def beauPrint(objStr):
 
 
 def fillingDeadlines(todayYear):
-    write("icit.html", "td", "dedline", "Исит (лаб) 11:45-13:20", todayYear)
-    write("programm.html", "td", "dedline", "Программирование (лаб) 9:45-11:20", todayYear)
+    write("Исит (лаб) 11:45-13:20", todayYear, parse(icit))
+    write("Программирование (лаб) 9:45-11:20", todayYear, parse(programm))
 
 
 @bot.message_handler(commands=['start'])
 def startWork(message):
+    authorization("stud67266", "bZqb11Fy")
+    global programm, icit
+    programm, icit = parseDedline("Программирование"), parseDedline("Исит")
     bot.send_message(message.chat.id, "Команды:\n1./Сегодня\n2./Завтра\n3./Неделя\n4./Неделя_сл")
 
 
@@ -88,9 +116,8 @@ def thisWeek(message):
 
 @bot.message_handler(commands=['Неделя_сл'])
 def nextWeek(message):
-    nextWeek = datetime.timedelta(days = 7) + datetime.date.today()
-    dNext = datetime.date(nextWeek.year, nextWeek.month, nextWeek.day)
-    fillingDeadlines(dNext)
+    nextWeek = datetime.timedelta(days = datetime.datetime.today().weekday() + 1) + datetime.date.today()
+    fillingDeadlines(nextWeek)
     buf = worksheet
     if determineParity(date.today()) == "Четная неделя":
         buf = sh.worksheet("Нечетная неделя")
